68ccdf2dd2948755bcd6acc1d83e89edee7523b2
Ebenezer Abiodun Sam-Oladapo <mrsamoladapo@gmail.com>
2026-09-12 13:43:32 +0100

    Move database infra to platform

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("{}", platform::health());
+use axum::Router;
+use axum::routing::get;
+use tracing::info;
+use platform::Config;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().with_env_filter("info").init();
+    info!("tracing initialized");
+    
+
+    let config = Config::from_env();
+    let address = Config::bind_address(&config);
+
+
+    let app = Router::new()
+        .route("/healthz", get(healthz))
+        .route("/readyz", get(readyz));
+
+    let listener = tokio::net::TcpListener::bind(&address)
+        .await
+        .expect("failed to bind TCP listener");
+
+    info!("API listening on {}", address);
+
+    axum::serve(listener, app)
+        .await
+        .expect("server failed");
+
+
+}
+
+async fn healthz() -> &'static str {
+    platform::health()
+}
+
+async fn readyz() -> &'static str {
+    platform::health()
 }
--- crates/platform/src/lib.rs
+++ crates/platform/src/lib.rs
@@ -1,3 +1,27 @@
+use std::env;
+use dotenvy::dotenv;
+
 pub fn health() -> &'static str {
     "platform ok"
+}
+
+pub struct Config {
+    pub port: u16,
+    pub host: String,
+}
+
+impl Config {
+    pub fn from_env() -> Self {
+        dotenv().ok();
+
+       // let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+        let host = env::var("HOST").unwrap();
+
+        let port: u16 = env::var("PORT").unwrap_or_else(|_| "8080".to_string()).parse::<u16>().expect("PORT must be a valid u16");
+        Self{host,port}
+    }
+
+    pub fn bind_address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
 }
